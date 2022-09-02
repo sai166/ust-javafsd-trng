5870c0b1a1e7880a280bcb835edee41ede890281
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-02 20:11:51 +0530

    Add files via upload

---
+++ JAVA/Forloop_ex1.java
@@ -0,0 +1,16 @@
+package com.ust;
+
+public class Forloop_ex1 {
+	public static void main(String args[]) {
+		//for loop
+		for(int i=1;i<=10;++i) {
+			//if value of i is between 4 and 9
+			//continue is excuted
+			if(i>4&&i<9) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
