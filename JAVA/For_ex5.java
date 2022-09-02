b634adcf354cfa2132da1fc461d598b734eee755
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-02 20:08:03 +0530

    Add files via upload

---
+++ JAVA/For_ex5.java
@@ -0,0 +1,15 @@
+package com.ust;
+
+public class For_ex5 {
+	public static void main(String[]args) {
+		//Create an array
+		int[] age= {12,4,5};
+		//loop through array
+		//using for loop
+		System.out.println("Using for loop:");
+		for(int i=0; i<age.length;i++) {
+			System.out.println(age[i]);
+		}
+	}
+
+}
