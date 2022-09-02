0a123de242a080bda4d4492fe2adb2837478aea6
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-02 20:27:19 +0530

    Add files via upload

---
+++ JAVA/While_exg2.java
@@ -0,0 +1,13 @@
+package com.ust;
+
+public class While_exg2 {
+public static void main(String args[]) {
+	//declare variable
+	int i=1,n=5;
+	//while loopp for 1 to 5
+	while(i<=n) {
+		System.out.println(i);
+		i++;
+	}
+}
+}
