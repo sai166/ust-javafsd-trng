b7b197df7845699710da745e24d970d0d9b45855
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-02 20:21:45 +0530

    Add files via upload

---
+++ JAVA/Switch_2.java
@@ -0,0 +1,21 @@
+package com.ust;
+
+public class Switch_2 {
+	public static void main(String args[]) {
+		int expression=9;
+		switch (expression) {
+		case 2:
+			System.out.println("Small Size");
+			break;
+			
+		case 3:
+			System.out.println("Large size");
+			break;
+			//default case
+			default:
+				System.out.println("Unknown size");
+		
+		}
+	}
+
+}
