84fb11b921b261c84e5babfe5dd84a3c5f076132
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-02 20:04:09 +0530

    Add files via upload

---
+++ JAVA/Dowhile_1.java
@@ -0,0 +1,16 @@
+package com.ust;
+
+public class Dowhile_1 {
+	public static void main(String args[]) {
+		//for loop
+		for(int i=0;i<10;++i){
+		//if the value of i is 5 the loop terminates
+		if(i ==5) {
+			break;
+		}
+		System.out.println(i);
+		}
+	}
+
+}
+
