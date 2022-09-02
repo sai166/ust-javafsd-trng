d2cbd777a66bfe6c71cc650b10ab1579a88ab509
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-02 20:23:51 +0530

    Add files via upload

---
+++ JAVA/Varaible_1.java
@@ -0,0 +1,27 @@
+package com.ust;
+/**
+ * 
+ * @author ustjavafsdb210
+ *
+ */
+ public class Varaible_1 {
+int age;     //instance variable
+String name;
+float salary;
+int count=10;
+
+	
+	public static void main(String[] args) {
+		String str="sai";    //local variable
+		// TODO Auto-generated method stub
+//code changes done by Nehla CR 101
+		
+		//end changes by nehla
+		}
+
+
+ 
+ class V{
+	 
+ }
+ }
