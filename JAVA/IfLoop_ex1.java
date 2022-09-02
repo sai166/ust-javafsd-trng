b2bb2f93cd26a2982110dc2bd686fe05ca767888
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-02 20:13:17 +0530

    Add files via upload

---
+++ JAVA/IfLoop_ex1.java
@@ -0,0 +1,19 @@
+package com.ust;
+
+public class IfLoop_ex1 {
+	public void main(String[] args) {
+		int number=10;
+		//check if number is less than 0
+		if(number<0) {
+			//check the number is less than 10
+			if(number < 0);{
+				System.out.println("The number is negative.");
+			}
+			System.out.println("statement outside if block");
+			
+			
+		}
+		
+	}
+
+}
