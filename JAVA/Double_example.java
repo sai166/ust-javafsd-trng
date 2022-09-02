f3f5d2fb374f58e22eae81e587c41d36a1c34005
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-02 20:03:30 +0530

    Add files via upload

---
+++ JAVA/Double_example.java
@@ -0,0 +1,22 @@
+package com.ust;
+
+class Double_example {
+	public static void main(String[]args) {
+		int[] numbers= {2,-9,0,5,12,-25,22,9,8,12};
+		int sum=0;
+		double average;
+		
+		//access all element using for each loop
+		//add each element in sum
+		for(int number: numbers) {
+			sum +=number;
+		}
+			//get the total number of elements
+			int arrayLength= numbers.length;
+			average = ((double)sum/(double)arrayLength);
+			System.out.println("Sum="+sum);
+			System.out.println("Average="+average);
+			
+			
+		}
+	}
