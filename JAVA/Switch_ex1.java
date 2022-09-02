822b7150522f2c240236c134477c2e7a885ed818
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-02 20:22:08 +0530

    Add files via upload

---
+++ JAVA/Switch_ex1.java
@@ -0,0 +1,22 @@
+package com.ust;
+
+public class Switch_ex1 {
+	public static void main() {
+		int expression=2;
+		//switch statement to check size
+		switch(expression) {
+		case 1:
+			System.out.println("case1");
+			//matching class
+		case 2:
+			System.out.println("case2");
+		case 3:
+			System.out.println("case3");
+			
+			default:
+				System.out.println("Default case");
+			
+		}
+	}
+
+}
