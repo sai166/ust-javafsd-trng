db95b5643ea93f02d968edef7779cb2401d7b82a
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-02 20:12:51 +0530

    Add files via upload

---
+++ JAVA/If_eg1.java
@@ -0,0 +1,13 @@
+package com.ust;
+
+public class If_eg1 {
+	public static void main(String[]args) {
+		int number=10;
+		if(number>0) {
+			System.out.println("The number is not positives.");
+			
+		}
+		System.out.println("Standard outside if...else block");
+	}
+
+}
