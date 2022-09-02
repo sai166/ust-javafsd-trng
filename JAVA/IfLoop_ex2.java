51f3130ad1aebce4c3e8b6d526c1ad60cbe38740
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-02 20:13:50 +0530

    Add files via upload

---
+++ JAVA/IfLoop_ex2.java
@@ -0,0 +1,20 @@
+package com.ust;
+
+public class IfLoop_ex2 {
+	public static void main(String args[]) {
+		int number=0;
+		if (number>0){
+			System.out.println("The is positive.");
+		}
+		else if(number<0){
+			System.out.println("The is negative.");
+		}
+		else {
+			System.out.println("The number is 0.");
+			
+			
+		}
+		
+	}
+
+}
