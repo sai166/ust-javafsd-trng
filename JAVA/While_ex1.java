33aa59d444f11bc8f9dafa282aaa8ac5217f6024
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-02 20:25:32 +0530

    Add files via upload

---
+++ JAVA/While_ex1.java
@@ -0,0 +1,15 @@
+package com.ust;
+
+
+public class While_ex1 {
+//program to display 1 to 5
+	public static void main(String args[]) {
+		//declare variable
+		int i=1,s=5;
+		//while loop from 1 to 5
+		while(i <=s) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
