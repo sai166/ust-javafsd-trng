727555b3d029d27c79f29fa594c1cae2d5df00da
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-02 20:17:18 +0530

    Add files via upload

---
+++ JAVA/Loop_5.java
@@ -0,0 +1,14 @@
+package com.ust;
+import java.util.Scanner;
+public class Loop_5 {
+//java program to display numbers from 1 to 5
+	public static void main(String args[]) {
+		int i=1,n=5;
+		//do..while loop from 1 to 5
+		do {
+			System.out.println(i);
+			i++;
+			
+		}while(i<=n);
+	}
+}
