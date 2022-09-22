4d4270e7d98058dceedac4dfd2fef69940fcaed6
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-22 14:41:41 +0530

    Add files via upload

---
+++ JAVA/Array/Array.java
@@ -0,0 +1,15 @@
+package com.ust;
+
+import java.util.Arrays;
+
+class Array {
+	public static void main(String args []) {
+		int[] numbers=new int[2];
+		numbers[0] =1;
+		numbers[1] =2;
+		
+		System.out.println(Arrays.toString(numbers));
+		
+	}
+
+}
---
+++ JAVA/Array/Array_java.java
@@ -0,0 +1,14 @@
+package com.ust;
+
+public class Array_java {
+	public static void main(String args[]) {
+int number =51;
+do {
+	System.out.println("sai is a good boy do while");
+	number--;
+}
+while (number> 0);
+		
+	}
+
+}
