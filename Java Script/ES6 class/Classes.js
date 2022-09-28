a2eaea0b6c35dd0c9a0d0022c24bbefa54d31b8f
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-28 15:20:31 +0530

    Add files via upload

---
+++ Java Script/ES6 class/Classes.js
@@ -0,0 +1,12 @@
+class Student{
+    constructor(name,age){
+        this.n=name;
+        this.a=age;
+    }
+    stu(){
+        console.log("The name of the student is  :",this.n)
+        console.log("The Age of the Student is:",this.a)
+    }
+}
+var stuObj=new Student('Peter',20);
+stuObj.stu();
