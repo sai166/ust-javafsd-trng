0cf11f7e6626f0cd73a99e7643aca2de8f4352d4
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-28 11:51:13 +0530

    Add files via upload

---
+++ Java Script/ES6/sample.js
@@ -0,0 +1,8 @@
+var name="sai"
+console.log(name)
+function 
+check(){
+    var y=100
+    console.log(y)
+}
+check()
---
+++ Java Script/ES6/sample1.js
@@ -0,0 +1,4 @@
+var num1=30;
+var num2=20;
+var res = num1>num2?"yes 30 is greater than 20":"no its not";
+console.log(res);
