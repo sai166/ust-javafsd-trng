10b236be658c7e5b3404ca979a30cee3d0a0ac10
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-28 15:17:28 +0530

    Add files via upload

---
+++ Java Script/Arrow function/ArrowFunction.js
@@ -0,0 +1,16 @@
+//function expressions
+var myfun1=function show(){
+
+}
+//Anomymous function
+ var myfun2=function(){
+     console.log("it is an Anonymous Function");
+
+ }
+ //Aroow function
+ var myfun3=()=>{
+     console.log("It is an arrow Function");
+ }
+ myfun1();
+ myfun2();
+ myfun3();
