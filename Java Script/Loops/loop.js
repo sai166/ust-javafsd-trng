052458ad1bb6b43397e3307bba05e8cfb458f332
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-28 15:14:39 +0530

    Add files via upload

---
+++ Java Script/Loops/loop.js
@@ -0,0 +1,4 @@
+var fruits=['Apple','Banana','Mango','Orange'];
+for(let value of fruits){
+    console.log(value);
+}
---
+++ Java Script/Loops/loops1.js
@@ -0,0 +1,10 @@
+var n=1;
+while(n<=7)
+{
+    console.log("n=n"+n);
+    if(n==4)
+    {
+        break;
+    }
+    n++;
+}
