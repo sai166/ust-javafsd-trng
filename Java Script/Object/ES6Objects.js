1d699df35aa7f0efa336cfb9848c8f549eeb3ac4
sai166 <112387531+sai166@users.noreply.github.com>
2022-09-28 15:19:40 +0530

    Add files via upload

---
+++ Java Script/Object/ES6Objects.js
@@ -0,0 +1,9 @@
+let fname='Anil',
+lname='Kumar';
+
+let user={
+    fname,
+    lname
+};
+console.log(user.fname);
+console.log(user.lname);
---
+++ Java Script/Object/ES6Objects2.js
@@ -0,0 +1,7 @@
+var department='dep_name';
+var emp={
+    id:102,
+    name:'Anil',
+    [department]:'Production'
+}
+console.log(emp);
