aa858e167c5b28379c8455921424440a5f7bbc46
sai166 <112387531+sai166@users.noreply.github.com>
2022-10-14 12:55:25 +0530

    Add files via upload

---
+++ SBA/Week6/Sample.java
@@ -0,0 +1,15 @@
+package ust.com;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class Sample {
+	
+	static Logger  log=Logger.getLogger(Sample.class.getName());
+	public static void main(String[] args) {
+		BasicConfigurator.configure();
+		log.debug("Heloo debug");
+		log.info("Hello from info");
+	}
+
+}
